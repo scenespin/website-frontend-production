d20cb05561c7922188829f88a95e63559a834d68
GardenSC <jeffrey.posner@gmail.com>
2025-10-30 17:33:41 -0400

    update auth components

--- components/ButtonSignin.js
+++ components/ButtonSignin.js
@@ -1,36 +1,44 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { useSession, signIn } from "next-auth/react";
+import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import config from "@/config";
 
-// A simple button to sign in with our providers (Google & Magic Links).
+// A simple button to sign in with Clerk.
 // It automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
 // If the user is already logged in, it will show their profile picture & redirect them to callbackUrl immediately.
 const ButtonSignin = ({ text = "Get started", extraStyle }) => {
   const router = useRouter();
-  const { data: session, status } = useSession();
+  const { user, isLoaded } = useUser();
 
   const handleClick = () => {
-    if (status === "authenticated") {
+    if (user) {
       router.push(config.auth.callbackUrl);
     } else {
-      signIn(undefined, { callbackUrl: config.auth.callbackUrl });
+      router.push("/sign-in");
     }
   };
 
-  if (status === "authenticated") {
+  if (!isLoaded) {
+    return (
+      <button className={`btn ${extraStyle ? extraStyle : ""}`} disabled>
+        <span className="loading loading-spinner loading-xs"></span>
+      </button>
+    );
+  }
+
+  if (user) {
     return (
       <Link
         href={config.auth.callbackUrl}
         className={`btn ${extraStyle ? extraStyle : ""}`}
       >
-        {session.user?.image ? (
+        {user.imageUrl ? (
           <img
-            src={session.user?.image}
-            alt={session.user?.name || "Account"}
+            src={user.imageUrl}
+            alt={user.fullName || "Account"}
             className="w-6 h-6 rounded-full shrink-0"
             referrerPolicy="no-referrer"
             width={24}
@@ -38,10 +46,10 @@ const ButtonSignin = ({ text = "Get started", extraStyle }) => {
           />
         ) : (
           <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
-            {session.user?.name?.charAt(0) || session.user?.email?.charAt(0)}
+            {user.firstName?.charAt(0) || user.emailAddresses[0]?.emailAddress?.charAt(0)}
           </span>
         )}
-        {session.user?.name || session.user?.email || "Account"}
+        {user.fullName || user.emailAddresses[0]?.emailAddress || "Account"}
       </Link>
     );
   }
--- components/LayoutClient.js
+++ components/LayoutClient.js
@@ -2,19 +2,18 @@
 
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import { useSession } from "next-auth/react";
+import { useUser } from "@clerk/nextjs";
 import { Crisp } from "crisp-sdk-web";
-import { SessionProvider } from "next-auth/react";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "react-hot-toast";
 import { Tooltip } from "react-tooltip";
 import config from "@/config";
 
 // Crisp customer chat support:
-// This component is separated from ClientLayout because it needs to be wrapped with <SessionProvider> to use useSession() hook
+// This component uses Clerk's useUser() hook to identify users
 const CrispChat = () => {
   const pathname = usePathname();
-  const { data } = useSession();
+  const { user } = useUser();
 
   useEffect(() => {
     if (config?.crisp?.id) {
@@ -37,46 +36,44 @@ const CrispChat = () => {
 
   // Add User Unique ID to Crisp to easily identify users when reaching support (optional)
   useEffect(() => {
-    if (data?.user && config?.crisp?.id) {
-      Crisp.session.setData({ userId: data.user?.id });
+    if (user && config?.crisp?.id) {
+      Crisp.session.setData({ userId: user.id });
     }
-  }, [data]);
+  }, [user]);
 
   return null;
 };
 
 // All the client wrappers are here (they can't be in server components)
-// 1. SessionProvider: Allow the useSession from next-auth (find out if user is auth or not)
-// 2. NextTopLoader: Show a progress bar at the top when navigating between pages
-// 3. Toaster: Show Success/Error messages anywhere from the app with toast()
-// 4. Tooltip: Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content=""
-// 5. CrispChat: Set Crisp customer chat support (see above)
+// 1. NextTopLoader: Show a progress bar at the top when navigating between pages
+// 2. Toaster: Show Success/Error messages anywhere from the app with toast()
+// 3. Tooltip: Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content=""
+// 4. CrispChat: Set Crisp customer chat support (see above)
+// Note: No SessionProvider needed - Clerk handles auth via ClerkProvider in layout.js
 const ClientLayout = ({ children }) => {
   return (
     <>
-      <SessionProvider>
-        {/* Show a progress bar at the top when navigating between pages */}
-        <NextTopLoader color={config.colors.main} showSpinner={false} />
+      {/* Show a progress bar at the top when navigating between pages */}
+      <NextTopLoader color={config.colors.main} showSpinner={false} />
 
-        {/* Content inside app/page.js files  */}
-        {children}
+      {/* Content inside app/page.js files  */}
+      {children}
 
-        {/* Show Success/Error messages anywhere from the app with toast() */}
-        <Toaster
-          toastOptions={{
-            duration: 3000,
-          }}
-        />
+      {/* Show Success/Error messages anywhere from the app with toast() */}
+      <Toaster
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
 
-        {/* Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content="" */}
-        <Tooltip
-          id="tooltip"
-          className="z-[60] !opacity-100 max-w-sm shadow-lg"
-        />
+      {/* Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content="" */}
+      <Tooltip
+        id="tooltip"
+        className="z-[60] !opacity-100 max-w-sm shadow-lg"
+      />
 
-        {/* Set Crisp customer chat support */}
-        <CrispChat />
-      </SessionProvider>
+      {/* Set Crisp customer chat support */}
+      <CrispChat />
     </>
   );
 };
