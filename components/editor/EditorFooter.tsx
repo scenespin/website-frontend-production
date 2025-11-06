ce183ac21b3cb84c85a9d5cf7ed64c997cfa4642
GardenSC <jeffrey.posner@gmail.com>
2025-11-06 14:43:33 -0500

    Remove duplicate keyboard shortcuts from editor footer

--- components/editor/EditorFooter.tsx
+++ components/editor/EditorFooter.tsx
@@ -3,31 +3,10 @@
 import React from 'react';
 
 /**
- * EditorFooter - Bottom bar showing keyboard shortcuts
+ * EditorFooter - Bottom bar (simplified - keyboard shortcuts shown in toolbar tooltips)
  * Theme-aware styling with DaisyUI classes
- * Hidden on mobile since keyboard shortcuts don't work on touch devices
  */
 export default function EditorFooter() {
-    return (
-        <div className="hidden md:block border-t border-base-300 bg-base-200 px-4 py-2 text-xs text-base-content/50">
-            <div className="flex flex-wrap gap-4">
-                <span>
-                    <kbd className="kbd kbd-sm bg-base-300 text-base-content/70">
-                        Enter
-                    </kbd> New line
-                </span>
-                <span>
-                    <kbd className="kbd kbd-sm bg-base-300 text-base-content/70">
-                        Tab
-                    </kbd> Character
-                </span>
-                <span>
-                    <kbd className="kbd kbd-sm bg-base-300 text-base-content/70">
-                        Shift+Tab
-                    </kbd> Scene heading
-                </span>
-            </div>
-        </div>
-    );
+    return null;
 }
 
