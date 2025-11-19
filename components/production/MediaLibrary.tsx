06e657efe0ea455b8cf21a0a607ddc633eb45696
GardenSC <jeffrey.posner@gmail.com>
2025-11-19 13:24:58 -0500

    Fix missing closing div for file item in MediaLibrary.tsx
    - Added missing closing </div> tag for file item div that opens at line 1104
    - File item div now properly closes before map function closes
    - All JSX elements now properly closed

--- components/production/MediaLibrary.tsx
+++ components/production/MediaLibrary.tsx
@@ -1246,10 +1246,11 @@ export default function MediaLibrary({
                           </DropdownMenuContent>
                         </DropdownMenu>
                       </div>
-                    ))}
-                  </div>
-                )}
-              </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
