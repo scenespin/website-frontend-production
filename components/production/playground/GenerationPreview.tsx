1e10eb97e0b62054d0fe34c6f7e1ad4ea297b276
Jeff <jeff@Jeffs-MacBook-Air.local>
2026-02-09 21:34:31 -0500

    fix(Video Gen): do not autoplay Latest generated video on tab load

--- components/production/playground/GenerationPreview.tsx
+++ components/production/playground/GenerationPreview.tsx
@@ -99,8 +99,8 @@ export function GenerationPreview({
                   src={generatedVideoUrl}
                   controls
                   className="w-full h-auto"
-                  autoPlay
                   loop
+                  playsInline
                 />
               )}
             </div>
